61578d1917b4d957825b4f83183f9d5ea6e8fa58
dom12 <matras60@gmail.com>
2026-01-29 21:18:40 +0100

    Learning CMake

---
+++ manager.cpp
@@ -0,0 +1,13 @@
+#include "include/nlohmann/json.hpp"
+#include "include/sodium.h"
+#include "include/sqlitle3.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+
+int main() {
+
+}
+
