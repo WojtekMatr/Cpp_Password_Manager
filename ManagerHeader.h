a67448c5cdaf10064d0f015326cdd2c1a9d9441f
dom12 <matras60@gmail.com>
2026-01-30 02:12:44 +0100

    CMake fixed & going better with manager

--- ManagerHeader.h
+++ ManagerHeader.h
@@ -1,9 +1,27 @@
 #pragma once
 #include <string>
+#include <nlohmann/json.hpp>
+#include <vector>
 
-
+struct UserPasswords;
 struct User {
+	std::vector<UserPasswords>* userPass;
+	std::string name;
+	unsigned char pwhash[32];
+	std::string email;
+	int iloscHasel;
+};
+struct UserPasswords {
 	std::string name;
 	std::string pass;
-	std::string email;
+	std::string site;
+};
+
+
+class JsonPasswords {
+	nlohmann::json* j;
+	std::vector<User>* users;
+
+public:
+
 };
--- manager.cpp
+++ manager.cpp
@@ -1,11 +1,12 @@
 #include "ManagerHeader.h"
 #include <nlohmann/json.hpp>
 #include <sodium.h>
-#include <sqlite3.h>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <memory>
+#include <ostream>
+#include <fstream>
 
 
 
@@ -13,56 +14,76 @@
 
 int main() {
 	std::cout << "Paswword manager -- Without GUI, next time i'll sit on this and learn Cpp Gui" << std::endl;
-
-	while (1) {
+	nlohmann::json j;
+	{
 		std::unique_ptr<User> user = std::make_unique<User>();
 		std::cout << "1. Log in" << std::endl;
 		std::cout << "2. Sign in" << std::endl;
 		std::cout << "3. Password reset" << std::endl;
 		int n;
 		std::cin >> n;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		system("cls");
-		switch (n) {
-		case 1:
+		{
+			std::unique_ptr<std::string> pass = std::make_unique<std::string>();
+			switch (n) {
+			case 1:
 
-			std::cout << "User name: " << std::endl;
-			std::getline(std::cin, (user->name));
-			std::cout << "Password: " << std::endl;
-			std::getline(std::cin, (user->pass));
-		case 2:
-			std::cout << "User name: " << std::endl;
-			std::getline(std::cin, (user->name));
-			std::cout << "Password: " << std::endl;
-			std::getline(std::cin, (user->pass));
-			std::cout << "Your email address: " << std::endl;
-			std::getline(std::cin, (user->name));
-			unsigned char salt[crypto_pwhash_SALTBYTES];//crypto_pwhash_SALTBYTES const from libsoduim tells us how many bytes supose have salt 
-			// in practice salt[0] - 1st byte salt[1] - 2nd byte...
-			randombytes_buf(salt, sizeof salt);
-			unsigned char pwhash[32];
+				std::cout << "User name: " << std::endl;
+				std::getline(std::cin, (user->name));
+				std::cout << "Password: " << std::endl;
+				std::getline(std::cin, (*pass));
+				break;
+			case 2:
+				std::cout << "User name: " << std::endl;
+				std::getline(std::cin, (user->name));
+				std::cout << "Password: " << std::endl;
+				std::getline(std::cin, (*pass));
+				std::cout << "Your email address: " << std::endl;
+				std::getline(std::cin, (user->name));
+				unsigned char salt[crypto_pwhash_SALTBYTES];//crypto_pwhash_SALTBYTES const from libsoduim tells us how many bytes supose have salt 
+				// in practice salt[0] - 1st byte salt[1] - 2nd byte...
+				randombytes_buf(salt, sizeof salt);
+				unsigned char pwhash[32];
 
-			crypto_pwhash(
-				pwhash,
-				sizeof pwhash,
-				user->pass.c_str(),
-				user->pass.size(),
-				salt,
-				crypto_pwhash_OPSLIMIT_MODERATE,
-				crypto_pwhash_MEMLIMIT_MODERATE,
-				crypto_pwhash_ALG_ARGON2ID13
-			);
-			std::cout << pwhash;
+				crypto_pwhash(
+					pwhash,
+					sizeof pwhash,
+					pass->c_str(),
+					pass->size(),
+					salt,
+					crypto_pwhash_OPSLIMIT_MODERATE,
+					crypto_pwhash_MEMLIMIT_MODERATE,
+					crypto_pwhash_ALG_ARGON2ID13
+				);
+				std::cout << pwhash<<std::endl;
+				memcpy(user->pwhash, pwhash, 32);
+				std::cout << *pass;
+				j["user"] = user->name;
+				j["kod"] = user->pwhash;
+				j["e-mail"] = user->email;
+
+				break;
+			case 3:
+				std::cout << "Reseting password!" << std::endl;
+				std::cout << "User name\n";
+				std::getline(std::cin, (user->name));
+				std::cout << """Email\n";
+				std::getline(std::cin, (user->email));
+				break;
 
 
 
 
 
-
-
-
+			}
+			std::ofstream file("output.json", std::ios::out | std::ios::trunc);
+			if (file.is_open()) {
+				file << j.dump(4); 
+				file.close();
+			}
 
 		}
-
 
 
 
